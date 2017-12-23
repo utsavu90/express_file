a57bd11d611278bb85ccf9a662a841596f8c286e
unknown <utsavkk90@gmail.com>
2017-12-23 23:50:07 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const express=require('express');
+const hbs=require('hbs');
+
+var app= express();
+const fs=require('fs');
+app.set('view engine','hbs');
+app.use(express.static(__dirname ));
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log=`now:${now}URL:${req.baseUrl} method:${req.method}`;
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    if(err)
+    console.log("unable to log server");
+  });
+  next();
+});
+/*app.get('/',(req,res)=>{
+ res.send({ name:'utsav',
+  likes:['business','music']
+});
+});*/
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('date',()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('upstream',(text)=>{
+  return text.toUpperCase();
+});
+app.get('/tree',(req,res/*,next*/)=>{
+  res.render('about.hbs',{credit:'some author',text:'urvashi krishnatra'});
+  //next();
+});
+
+
+                            app.use('/', function(req, res){
+                               console.log('End');
+                            });
+
+app.get('/blue',(req,res)=>{
+    res.render('rexa.hbs',{credit:'some author'});
+});
+app.get('/',(req,res)=>{res.send({great:"mom n dad r great!"})});
+app.listen(3000);
+/*app.listen(3000,(req,res)=>{
+  console.log("server on port:3000"+__dirname);
+});*/
